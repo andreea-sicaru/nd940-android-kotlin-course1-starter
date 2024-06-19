39fbd56e22911ea28c564a38a8211e4ac5132508
Andreea Sicaru <andreea.sicaru@uber.com>
2024-06-19 20:52:36 +0300

    Add logout logic

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,15 +3,36 @@ package com.udacity.shoestore
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+//    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+
+
+
+//        drawerLayout = binding.drawerLayout
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+//        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+//        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+//        NavigationUI.setupWithNavController(binding.navView, navController)
 
         // Initialize Timber
         Timber.plant(Timber.DebugTree())
--- starter/app/src/main/java/com/udacity/shoestore/shoelist/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/shoelist/ShoeListFragment.kt
@@ -2,6 +2,9 @@ package com.udacity.shoestore.shoelist
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -9,15 +12,13 @@ import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoelistFragmentBinding
-import timber.log.Timber
+import com.udacity.shoestore.models.Shoe
 
 class ShoeListFragment : Fragment() {
 
-//    private val viewModel = activityViewModels<ShoeListViewModel>().value
     private val viewModel: ShoeListViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -26,34 +27,55 @@ class ShoeListFragment : Fragment() {
             inflater, R.layout.shoelist_fragment, container, false
         )
 
-//        viewModel = ViewModelProvider(this)[ShoeListViewModel::class.java]
-
         var args = ShoeListFragmentArgs.fromBundle(requireArguments())
         addShoeFromArgs(args)
 
         viewModel.shoeList.observe(viewLifecycleOwner) { shoeList ->
             shoeList.forEach { shoe ->
-                Timber.i("Shoe: ${shoe.name}")
-                val shoeCard = inflater.inflate(R.layout.shoe_card, container, false) as CardView
-                shoeCard.findViewById<TextView>(R.id.shoecard_name).text = shoe.name
-                shoeCard.findViewById<TextView>(R.id.shoecard_size).text = shoe.size.toString()
-                shoeCard.findViewById<TextView>(R.id.shoecard_company).text = shoe.company
-                shoeCard.findViewById<TextView>(R.id.shoecard_description).text = shoe.description
+                val shoeCard = createShoeCard(inflater, container, shoe)
                 binding.shoeLayout.addView(shoeCard)
             }
         }
-
 
         binding.addShoeButton.setOnClickListener {
             findNavController().navigate(ShoeListFragmentDirections.actionShoeListFragmentToShoeAddFragment())
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.logout, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                findNavController().navigate(ShoeListFragmentDirections.actionShoeListFragmentToLoginFragment())
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun addShoeFromArgs(args: ShoeListFragmentArgs) {
         if (args.shoeName != "" && args.shoeSize != 0 && args.shoeCompany != "" && args.shoeDescription != "") {
             viewModel.addShoe(args.shoeName, args.shoeSize, args.shoeCompany, args.shoeDescription)
         }
+    }
+
+    private fun createShoeCard(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        shoe: Shoe
+    ): CardView {
+        val shoeCard = inflater.inflate(R.layout.shoe_card, container, false) as CardView
+        shoeCard.findViewById<TextView>(R.id.shoecard_name).text = shoe.name
+        shoeCard.findViewById<TextView>(R.id.shoecard_size).text = shoe.size.toString()
+        shoeCard.findViewById<TextView>(R.id.shoecard_company).text = shoe.company
+        shoeCard.findViewById<TextView>(R.id.shoecard_description).text = shoe.description
+        return shoeCard
     }
 }
