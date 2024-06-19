664712729b1a5d4685385044a4a504bf162a68cb
Andreea Sicaru <andreea.sicaru@uber.com>
2024-06-19 18:09:59 +0300

    Add shoes to Shoe List on save

--- starter/app/src/main/java/com/udacity/shoestore/shoelist/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/shoelist/ShoeListFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
@@ -14,28 +17,31 @@ import timber.log.Timber
 
 class ShoeListFragment : Fragment() {
 
-    private lateinit var viewModel: ShoeListViewModel
+//    private val viewModel = activityViewModels<ShoeListViewModel>().value
+    private val viewModel: ShoeListViewModel by activityViewModels()
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         val binding: ShoelistFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.shoelist_fragment, container, false
         )
 
-        viewModel = ViewModelProvider(this)[ShoeListViewModel::class.java]
-
-//        viewModel.shoeList.observe(viewLifecycleOwner, { shoeList ->
-//            binding.shoeLayout.removeAllViews()
-////            shoeList.
-////            shoeList.forEach { shoe ->
-////                Timber.i("${shoe.name} ${shoe.size} ${shoe.company} ${shoe.description}")
-////            }
-//        }
+//        viewModel = ViewModelProvider(this)[ShoeListViewModel::class.java]
 
         var args = ShoeListFragmentArgs.fromBundle(requireArguments())
-        Timber.i("${args.shoeName} ${args.shoeSize} ${args.shoeCompany} ${args.shoeDescription}")
+        addShoeFromArgs(args)
+
+        viewModel.shoeList.observe(viewLifecycleOwner) { shoeList ->
+            shoeList.forEach { shoe ->
+                Timber.i("Shoe: ${shoe.name}")
+                val shoeCard = inflater.inflate(R.layout.shoe_card, container, false) as CardView
+                shoeCard.findViewById<TextView>(R.id.shoecard_name).text = shoe.name
+                shoeCard.findViewById<TextView>(R.id.shoecard_size).text = shoe.size.toString()
+                shoeCard.findViewById<TextView>(R.id.shoecard_company).text = shoe.company
+                shoeCard.findViewById<TextView>(R.id.shoecard_description).text = shoe.description
+                binding.shoeLayout.addView(shoeCard)
+            }
+        }
 
 
         binding.addShoeButton.setOnClickListener {
@@ -43,5 +49,11 @@ class ShoeListFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun addShoeFromArgs(args: ShoeListFragmentArgs) {
+        if (args.shoeName != "" && args.shoeSize != 0 && args.shoeCompany != "" && args.shoeDescription != "") {
+            viewModel.addShoe(args.shoeName, args.shoeSize, args.shoeCompany, args.shoeDescription)
+        }
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/shoelist/ShoeListViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/shoelist/ShoeListViewModel.kt
@@ -10,4 +10,13 @@ class ShoeListViewModel : ViewModel() {
     private val _shoeList = MutableLiveData<List<Shoe>>()
     val shoeList: LiveData<List<Shoe>>
         get() = _shoeList
+
+    fun addShoe(shoeName: String, shoeSize: Int, shoeCompany: String, shoeDescription: String) {
+        _shoeList.value = _shoeList.value.orEmpty() + Shoe(
+            shoeName,
+            shoeSize.toDouble(),
+            shoeCompany,
+            shoeDescription
+        )
+    }
 }
